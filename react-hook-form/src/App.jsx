a312f3317cec33d7b2dea68aa55da21fd7962e8c
arunbababa <hatuki.1.gzs@icloud.com>
2025-03-18 09:57:17 +0900

    ちょい検証途中、次register fieldsやる

--- react-hook-form/src/App.jsx
+++ react-hook-form/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// つまるところカスタムフック
+import { useForm } from "react-hook-form";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
+  // この関数は、フォームが送信されたときに呼び出されます。
+  const onSubmit = data => console.log(data);
+
+  // 見てわかるが、register は input 要素に対して、名前付けもできて、その名前を watch で監視できる
+  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("exampleRequired")); // watch input value by passing the name of it
+  console.log(watch("myInput")); // watch input value by passing the name of it
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+    // memo : dateはonSubmit時にformのvalueを取得する
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+      
+      {/* include validation with required or other standard HTML validation rules */}
+      <input defaultValue="exampleRequired" {...register("exampleRequired", { required: true, minLength: 10 })} />
+      
+      <input defaultValue="myInput" {...register("myInput", { required: true })} />
+
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>exampleRequired is required</span>}
+      <br />
+      {errors.myInput && <span>myInput is required</span>}
+      <br />
+      {errors.exampleRequired && <span>exampleRequired is minLength</span>}
+      <br />
+      <input type="submit" />
+    </form>
+  );
+}
